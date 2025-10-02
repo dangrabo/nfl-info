f7c9c991310ec08866c8fe43448fa8a8a999fa5b
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-02 13:44:49 -0700

    created header componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from './components/Header.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./nfl.svg" alt="nfl-logo" id="header-logo"/>
+            <h1>NFL Info Page</h1>
+        </header>
+    )
+}
